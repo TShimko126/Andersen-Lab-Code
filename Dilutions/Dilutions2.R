cd2df4de54c747282fb5f035b5aa234bac27b6f4
TShimko126 <TShimko126@gmail.com>
2013-05-30 14:50:15 -0500

    Updates to Dilutions2 R script

--- Dilutions/Dilutions2.R
+++ Dilutions/Dilutions2.R
@@ -53,13 +53,20 @@ date = Sys.Date()
 date = as.character(format(date, format = "%m%d%Y"))
 
 recipe = as.data.frame(makeDilute(dilutes,stock))
-colnames(recipe) = c("Diluent", "Conc. (µM)","S med. (µL)", paste(compound, "at", stock/1000, "mM"), "100 mg/mL Bact. Lysate (µL)")
+diluentAmount = c()
+for (i in seq(1,length(recipe[,1]))){
+  diluentAmount[i] = max(as.numeric(as.character(recipe[,4])))-as.numeric(as.character(recipe$ul[i]))
+}
+recipe = cbind(recipe, diluentAmount)
+recipe$solv = rep(min(as.numeric(as.character(recipe$solv)))+120, length(recipe[,1]))
 
-saveRDS(stockmake,file="/Users/tylershimko/Andersen-Lab-Code/Dilutions/stockmake.rds")
-saveRDS(recipe,file="/Users/tylershimko/Andersen-Lab-Code/Dilutions/recipe.rds")
-saveRDS(compound,file="/Users/tylershimko/Andersen-Lab-Code/Dilutions/compound.rds")
+colnames(recipe) = c("Diluent", "   Conc. (µM)","   S med./Lysate Mix (µL)", paste("   ", compound, "at", stock/1000, "mM"), "   100 mg/mL Bact. Lysate (µL)", paste("   ", recipe$solvent[1],"(µL)"))
 
-#filename = paste0("/Users/tylershimko/Desktop/Dilutions/", compound, "_", date, ".txt")
-#file.create(filename)
-#capture.output(print(recipe, print.gap=3), file=filename)
-#write.table(recipe, file = filename, sep = "\t")
+saveRDS(stockmake,file="stockmake.rds")
+saveRDS(recipe,file="recipe.rds")
+saveRDS(compound,file="compound.rds")
+
+require(knitr) # required for knitting from rmd to md
+require(markdown) # required for md to html 
+knit('Dilutions2.rmd', 'Dilutions2.md') # creates md file
+markdownToHTML('Dilutions2.md', paste0(compound, "_", date, ".html")) # creates html file
